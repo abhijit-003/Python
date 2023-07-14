9b25d301881afca3c81f2eab795c16068c12f3cb
Abhijit <129248907+abhijit-003@users.noreply.github.com>
2023-07-14 09:04:58 +0530

    Implementation of Higher order function

---
+++ Python Functions/higher order function.py
@@ -0,0 +1,7 @@
+#higher order function
+def square(x):
+    return x ** 2
+
+numbers = [1, 2, 3, 4, 5]
+squared_numbers = map(square, numbers)
+print(list(squared_numbers))  # Output: [1, 4, 9, 16, 25]
